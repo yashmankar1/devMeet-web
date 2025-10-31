8285710579587c69df0f81785f7691d5a5c9383c
yashmankar1 <deep.mankar@ranium.in>
2025-10-31 14:58:51 +0530

    feat: enhance Feed and UserCard components; add user request handling and no feed message

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,6 +25,10 @@ const Feed = () => {
   useEffect(() => {
     getFeed();
   }, []);
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return <h1 className=" flex justify-center  my-10">No new users found!</h1>;
   return (
     feed && (
       <div className="flex justify-center my-10">
--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -6,10 +6,13 @@ const feedSlice = createSlice({
   reducers: {
     addFeed: (state, action) => action.payload,
 
-    removeFeed: (state, action) => null,
+    removeUserFromFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);
+      return newFeed;
+    },
   },
 });
 
-export const { addFeed } = feedSlice.actions;
+export const { addFeed, removeUserFromFeed } = feedSlice.actions;
 export default feedSlice.reducer;
  
