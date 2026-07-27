6c7f26e8888ceb3521ae4691423508666aa367f0
Yash Mankar <yash@devmeet.dev>
2026-07-27 12:16:52 +0000

    fix: point socket.io to Render backend URL instead of Vercel

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,9 +2,5 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/api/socket.io" });
-  }
+  return io(BASE_URL, { withCredentials: true });
 };
