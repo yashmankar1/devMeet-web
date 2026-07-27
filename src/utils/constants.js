9161a7454edc8b34b11292c0aa24e9af86a4c155
yashmankar1 <yashmankar349@gmail.com>
2026-07-27 17:31:31 +0530

    fix: update BASE_URL to point to Render backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
-
-
-  
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://devmeet-x4ay.onrender.com";
